d4fc08e946d640dd4ad74961339a156a2edbf2d5
James Yang <jamesyang40k@gmail.com>
2026-06-01 15:15:59 +0900

    add label smoothing

--- main.py
+++ main.py
@@ -156,7 +156,7 @@ def train(args, epoch, model, scaler, amp_context, optimizer, schedule, train_lo
                     if args.lambda_s != 0:
                         S = outs[name].size(1)
                         logit = outs[name].view(-1, args.num_classes).contiguous()
-                        loss_s = nn.CrossEntropyLoss()(logit, 
+                        loss_s = nn.CrossEntropyLoss(label_smoothing=getattr(args, 'label_smoothing', 0.0))(logit, 
                                                        labels.unsqueeze(1).repeat(1, S).flatten(0))
                         loss += args.lambda_s * loss_s
                     else:
@@ -182,7 +182,7 @@ def train(args, epoch, model, scaler, amp_context, optimizer, schedule, train_lo
                         raise ValueError("FPN not use here.")
                     if args.lambda_b != 0:
                         ### here using 'layer1'~'layer4' is default setting, you can change to your own
-                        loss_b = nn.CrossEntropyLoss()(outs[name].mean(1), labels)
+                        loss_b = nn.CrossEntropyLoss(label_smoothing=getattr(args, 'label_smoothing', 0.0))(outs[name].mean(1), labels)
                         loss += args.lambda_b * loss_b
                     else:
                         loss_b = 0.0
@@ -192,7 +192,7 @@ def train(args, epoch, model, scaler, amp_context, optimizer, schedule, train_lo
                         raise ValueError("Combiner not use here.")
 
                     if args.lambda_c != 0:
-                        loss_c = nn.CrossEntropyLoss()(outs[name], labels)
+                        loss_c = nn.CrossEntropyLoss(label_smoothing=getattr(args, 'label_smoothing', 0.0))(outs[name], labels)
                         loss += args.lambda_c * loss_c
                     # combiner 기준 예측값 누적 (Precision/Recall/F1용)
                     with torch.no_grad():
@@ -201,7 +201,7 @@ def train(args, epoch, model, scaler, amp_context, optimizer, schedule, train_lo
                         all_train_labels.extend(labels.cpu().tolist())
 
                 elif "ori_out" in name:
-                    loss_ori = F.cross_entropy(outs[name], labels)
+                    loss_ori = F.cross_entropy(outs[name], labels, label_smoothing=getattr(args, 'label_smoothing', 0.0))
                     loss += loss_ori
             
             loss /= args.update_freq
